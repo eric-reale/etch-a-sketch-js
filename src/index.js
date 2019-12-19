10b1374cde77d9841ebaff8b4195f8e5e7eb7654
Eric Reale <ecr5083@gmail.com>
2019-12-19 10:13:54 -0500

    etch-a-sketch complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,82 @@
-console.log("Hello from src/index.js!");
-
-const id = document.getElementById('#id');
-
 const canvas = document.getElementById('etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 10;
+
+// const width = canvas.width;
+// const height = canvas.height;
+
+// OR write it by destructuring:
+const { width, height } = canvas;
+
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = MOVE_AMOUNT;
+
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+ctx.beginPath(); // start the drawing
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
+
+// write a draw function
+function draw({ key }) {
+  // increment the hue
+  hue += 5;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  // console.log(key);
+  // start the path
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  // move our x and y values depending on what the user did
+  switch (key) {
+    case 'ArrowUp':
+      y -= MOVE_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x += MOVE_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y += MOVE_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x -= MOVE_AMOUNT;
+      break;
+    default:
+      break;
+  }
+
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+
+// write a handler for the keys
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault(); // to prevent arrow keys from scrolling the page up and down
+    draw({ key: e.key });
+  }
+}
+
+// clear shake/function
+function clearCanvas() {
+  canvas.classList.add('shake');
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    'animationend',
+    function() {
+      // console.log('Done the shake!');
+      canvas.classList.remove('shake');
+    },
+    { once: true }
+  );
+}
+
+window.addEventListener('keydown', handleKey);
+shakebutton.addEventListener('click', clearCanvas);
