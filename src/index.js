51f237c0988220ce41b29383ef5f189f9e8854e7
Eric Reale <ecr5083@gmail.com>
2019-12-19 09:03:44 -0500

    initialized etch a sketch project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log("Hello from src/index.js!");
+
+const id = document.getElementById('#id');
+
+const canvas = document.getElementById('etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakebutton = document.querySelector('.shake');
